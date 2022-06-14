a0e47474a46d7b174c43c45e0fb79fbf7c9dd9ce
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-14 20:41:36 +0300

    Добавьте файлы проекта.

---
+++ Task18_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int jamp(int n)
+{
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+
+    return jamp(n - 1) + jamp(n - 2) + jamp(n - 3);
+}
+int main()
+{
+    int n = 3;
+    std::cout << jamp(n);
+}
